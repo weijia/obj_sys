de5b87300db470ac5f1f110b78cbbae7602a3395
Richard <richardwangwang@gmail.com>
2015-01-11 17:28:01 +0800

    Added normal admin for ufs obj.

--- obj_sys/admin.py
+++ obj_sys/admin.py
@@ -3,7 +3,26 @@ import models_obj_rel
 from models_mptt import UfsObjInTree
 from django.contrib import admin
 from mptt.admin import MPTTModelAdmin
+from django.conf import settings
+
+"""
+if "guardian" in settings.INSTALLED_APPS:
+    from guardian.admin import GuardedModelAdmin as SingleModelAdmin
+else:
+    from django.contrib.admin import ModelAdmin as SingleModelAdmin
+"""
 
 
-register_all_in_module(models_obj_rel)
+class UfsObjAdmin(object):
+    def queryset(self, request):
+        qs = super(UfsObjAdmin, self).queryset(request)
+
+        # If super-user, show all comments
+        if request.user.is_superuser:
+            return qs
+
+        return qs.filter(user=request.user)
+
+
+register_all_in_module(models_obj_rel, admin_class_list=[UfsObjAdmin])
 admin.site.register(UfsObjInTree, MPTTModelAdmin)
