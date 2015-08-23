6233579e4271454ed5002f806848536ac8a78e87
Richard <richardwangwang@gmail.com>
2015-08-23 16:02:33 +0800

    Code refine.

--- obj_sys/admin.py
+++ obj_sys/admin.py
@@ -1,28 +1,38 @@
+from djangoautoconf.auto_conf_admin_tools.admin_register import AdminRegister
 from djangoautoconf.auto_conf_admin_utils import register_all_in_module
 import models_obj_rel
 from models_mptt import UfsObjInTree
 from django.contrib import admin
 from mptt.admin import MPTTModelAdmin
 from django.conf import settings
+#
+# """
+# if "guardian" in settings.INSTALLED_APPS:
+#     from guardian.admin import GuardedModelAdmin as SingleModelAdmin
+# else:
+#     from django.contrib.admin import ModelAdmin as SingleModelAdmin
+# """
+#
+#
+# class UfsObjAdmin(object):
+#     def queryset(self, request):
+#         qs = super(UfsObjAdmin, self).queryset(request)
+#
+#         # If super-user, show all comments
+#         if request.user.is_superuser:
+#             return qs
+#
+#         return qs.filter(user=request.user)
+#
+#
+# register_all_in_module(models_obj_rel, admin_class_list=[UfsObjAdmin])
+r = AdminRegister()
+r.register_all_models(models_obj_rel)
 
-"""
-if "guardian" in settings.INSTALLED_APPS:
-    from guardian.admin import GuardedModelAdmin as SingleModelAdmin
-else:
-    from django.contrib.admin import ModelAdmin as SingleModelAdmin
-"""
+
+# from reversion.helpers import patch_admin
+#
+# patch_admin(models_obj_rel.UfsObj)
 
 
-class UfsObjAdmin(object):
-    def queryset(self, request):
-        qs = super(UfsObjAdmin, self).queryset(request)
-
-        # If super-user, show all comments
-        if request.user.is_superuser:
-            return qs
-
-        return qs.filter(user=request.user)
-
-
-register_all_in_module(models_obj_rel, admin_class_list=[UfsObjAdmin])
 admin.site.register(UfsObjInTree, MPTTModelAdmin)
--- obj_sys/urls.py
+++ obj_sys/urls.py
@@ -1,8 +1,10 @@
-#from django.conf.urls import patterns, include, url
-#import libsys
+# from django.conf.urls import patterns, include, url
+# import libsys
 from django.conf.urls import patterns, include, url
 from django.contrib.auth.decorators import login_required
 from django.views.generic import ListView
+from towel.modelview import ModelView
+from models import UfsObj
 from tagging.models import Tag
 from api import UfsObjResource
 from add_tag_template_view import AddTagTemplateView
@@ -11,38 +13,46 @@ from api_ufs_obj_in_tree import UfsObjInTreeResource
 from ufs_obj_in_tree_view import UfsObjInTreeView
 from rss import LatestEntriesFeed
 
-
 ufs_obj_resource = UfsObjResource()
 ufs_obj_in_tree_resource = UfsObjInTreeResource()
-#tag_resource = TagResource()
+# tag_resource = TagResource()
+
+objs_views = ModelView(UfsObj)
+
+# resource_views_ajax = ModelView(BookableResource, base_template="modal.html")
+# resource_booking_req_views_ajax = ModelView(BookingRequest, base_template="modal.html")
+
+
 
 urlpatterns = patterns('',
-    url(r'^tagging/$', login_required(AddTagTemplateView.as_view())),
-    url(r'^tagging_local/$', login_required(AddTagTemplateViewLocal.as_view())),
-    url(r'^tagging/(?P<news_item_pk>\d+)/$', login_required(AddTagTemplateView.as_view()), name="news-item"),
-    url(r'^manager/$', 'obj_sys.views.manager'),
-    url(r'^listing/$', 'obj_sys.views.listing'),
-    url(r'^homepage/$', 'obj_sys.views.listing_with_description'),
-    (r'^latest/feed/$', LatestEntriesFeed()),
-    #url(r'^qrcode/$', 'thumbapp.views.gen_qr_code'),
-    #url(r'^image/$', 'thumbapp.views.image'),
-    url(r'^append_tags/$', 'obj_sys.obj_tagging.handle_append_tags_request'),
-    url(r'^query/$', 'obj_sys.views.query'),
-    url(r'^operations/', 'obj_sys.views.do_operation'),
-    url(r'^do_json_operation/$', 'obj_sys.views.do_json_operation'),
-    url(r'^remove_tag/$', 'obj_sys.obj_tagging.remove_tag'),
-    url(r'^add_tag/$', 'obj_sys.obj_tagging.add_tag'),
-    url(r'^get_tags/$', 'obj_sys.obj_tagging.get_tags'),
-    url(r'^tag_list/$', ListView.as_view(
-            queryset=Tag.objects.all(),
-            context_object_name='tagged_items',
-            template_name='obj_sys/pane.html')),
-    (r'^api/ufsobj/', include(ufs_obj_resource.urls)),
-    (r'^api/ufs_obj_in_tree/', include(ufs_obj_in_tree_resource.urls)),
-    (r'^tree_raw/', UfsObjInTreeView.as_view(template_name='obj_sys/mptt_tree.html')),
-    (r'^tree/', UfsObjInTreeView.as_view(template_name='obj_sys/mptt_tree_content_only.html')),
-    #(r'^api/tag/', include(tag_resource.urls)),
-    #url(r'^$', 'desktop.filemanager.views.index'),
-    #url(r'^.+$', 'desktop.filemanager.views.handler'),
-    #url(r'^homepage_all/$', 'obj_sys.views.homepage'),
-)
+                       url(r'^tagging/$', login_required(AddTagTemplateView.as_view())),
+                       url(r'^tagging_local/$', login_required(AddTagTemplateViewLocal.as_view())),
+                       url(r'^tagging/(?P<news_item_pk>\d+)/$', login_required(AddTagTemplateView.as_view()),
+                           name="news-item"),
+                       url(r'^manager/$', 'obj_sys.views.manager'),
+                       url(r'^listing/$', 'obj_sys.views.listing'),
+                       url(r'^homepage/$', 'obj_sys.views.listing_with_description'),
+                       (r'^latest/feed/$', LatestEntriesFeed()),
+                       # url(r'^qrcode/$', 'thumbapp.views.gen_qr_code'),
+                       # url(r'^image/$', 'thumbapp.views.image'),
+                       url(r'^append_tags/$', 'obj_sys.obj_tagging.handle_append_tags_request'),
+                       url(r'^query/$', 'obj_sys.views.query'),
+                       url(r'^operations/', 'obj_sys.views.do_operation'),
+                       url(r'^do_json_operation/$', 'obj_sys.views.do_json_operation'),
+                       url(r'^remove_tag/$', 'obj_sys.obj_tagging.remove_tag'),
+                       url(r'^add_tag/$', 'obj_sys.obj_tagging.add_tag'),
+                       url(r'^get_tags/$', 'obj_sys.obj_tagging.get_tags'),
+                       url(r'^tag_list/$', ListView.as_view(
+                           queryset=Tag.objects.all(),
+                           context_object_name='tagged_items',
+                           template_name='obj_sys/pane.html')),
+                       (r'^api/ufsobj/', include(ufs_obj_resource.urls)),
+                       (r'^api/ufs_obj_in_tree/', include(ufs_obj_in_tree_resource.urls)),
+                       (r'^tree_raw/', UfsObjInTreeView.as_view(template_name='obj_sys/mptt_tree.html')),
+                       (r'^tree/', UfsObjInTreeView.as_view(template_name='obj_sys/mptt_tree_content_only.html')),
+                       # (r'^api/tag/', include(tag_resource.urls)),
+                       # url(r'^$', 'desktop.filemanager.views.index'),
+                       # url(r'^.+$', 'desktop.filemanager.views.handler'),
+                       # url(r'^homepage_all/$', 'obj_sys.views.homepage'),
+                        url(r'^ufs/', include(objs_views.urls)),
+                       )
