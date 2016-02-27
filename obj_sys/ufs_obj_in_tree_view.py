d9cd03807aa0f61c08882c35aa3587dc4e004db1
Richard <richardwangwang@gmail.com>
2016-02-27 23:16:34 +0800

    Code refine for ufs tree view.

--- obj_sys/ufs_obj_in_tree_view.py
+++ obj_sys/ufs_obj_in_tree_view.py
@@ -4,33 +4,47 @@ from djangoautoconf.django_utils import retrieve_param
 from models_mptt import UfsObjInTree
 
 
-class ItemTreeView(TemplateView):
+class TreeView(TemplateView):
     item_class = UfsObjInTree
     default_level = 2
-    ufs_obj_type = None
+
+    def __init__(self, **kwargs):
+        super(TreeView, self).__init__(**kwargs)
+        self.tree_items = None
 
     def get_context_data(self, **kwargs):
         # context = super(AddTagTemplateView, self).get_context_data(**kwargs)
         context = {}
-        data = retrieve_param(self.request)
-        if "root" in data:
-            root_pk = data["root"]
-            tree_items = self.get_ufs_children(root_pk)
-        else:
-            tree_items = self.item_class.objects.filter(level__lt=self.default_level)
-
-        if not self.ufs_obj_type is None:
-            tree_items = tree_items.filter(ufs_obj_type=self.ufs_obj_type)
-
-        c = {"user": self.request.user, "nodes": tree_items}
+        self.init_tree_items()
+        self.get_final_query_set()
+        c = {"user": self.request.user, "nodes": self.tree_items}
         c.update(csrf(self.request))
         context.update(c)
         # log = logging.getLogger(__name__)
         # log.error(context)
         return context
 
-    def get_ufs_children(self, root_pk):
+    def get_final_query_set(self):
+        pass
+
+    def init_tree_items(self):
+        data = retrieve_param(self.request)
+        if "root" in data:
+            root_pk = data["root"]
+            self.tree_items = self.get_children(root_pk)
+        else:
+            self.tree_items = self.item_class.objects.filter(level__lt=self.default_level)
+
+    def get_children(self, root_pk):
         root = self.item_class.objects.filter(pk=root_pk)
         tree_items = self.item_class.objects.get_queryset_descendants(root).filter(
                 level__lt=root[0].level + self.default_level + 1)
         return tree_items
+
+
+class ItemTreeView(TreeView):
+    ufs_obj_type = None
+
+    def get_final_query_set(self):
+        if self.ufs_obj_type is not None:
+            self.tree_items = self.tree_items.filter(ufs_obj_type=self.ufs_obj_type)
