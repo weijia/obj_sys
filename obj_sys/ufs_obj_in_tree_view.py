92cea0489db7c9ae83ff13f4dd2c0de14e5c6994
Richard <richardwangwang@gmail.com>
2016-04-11 22:32:26 +0800

    Move TreeView out. Code refine.

--- obj_sys/ufs_obj_in_tree_view.py
+++ obj_sys/ufs_obj_in_tree_view.py
@@ -1,50 +1,10 @@
-from django.core.context_processors import csrf
-from django.views.generic import TemplateView
-from djangoautoconf.django_utils import retrieve_param
+from mptt_tree_view.views import TreeView
 from obj_sys.models_ufs_obj import UfsObj
-
-
-class TreeView(TemplateView):
-    item_class = UfsObj
-    default_level = 2
-
-    def __init__(self, **kwargs):
-        super(TreeView, self).__init__(**kwargs)
-        self.tree_items = None
-        self.root_pk = -1
-
-    def get_context_data(self, **kwargs):
-        # context = super(AddTagTemplateView, self).get_context_data(**kwargs)
-        context = {}
-        self.init_tree_items()
-        self.get_final_query_set()
-        c = {"user": self.request.user, "nodes": self.tree_items}
-        c.update(csrf(self.request))
-        context.update(c)
-        # log = logging.getLogger(__name__)
-        # log.error(context)
-        return context
-
-    def get_final_query_set(self):
-        pass
-
-    def init_tree_items(self):
-        data = retrieve_param(self.request)
-        if "root" in data:
-            self.root_pk = data["root"]
-            self.tree_items = self.get_children(self.root_pk)
-        else:
-            self.tree_items = self.item_class.objects.filter(level__lt=self.default_level)
-
-    def get_children(self, root_pk):
-        root = self.item_class.objects.filter(pk=root_pk)
-        tree_items = self.item_class.objects.get_queryset_descendants(root).filter(
-                level__lt=root[0].level + self.default_level + 1)
-        return tree_items
 
 
 class ItemTreeView(TreeView):
     ufs_obj_type = None
+    item_class = UfsObj
 
     def get_final_query_set(self):
         if self.ufs_obj_type is not None:
