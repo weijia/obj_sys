510063159145cd3fdc7bdd0c8b93dc46d98a88c8
richard <richardwangwang@gmail.com>
2016-04-25 17:54:52 +0800

    Remove not used app config.

--- obj_sys/models.py
+++ obj_sys/models.py
@@ -3,21 +3,8 @@ from models_obj_rel import *
 from models_ufs_obj import *
 
 
-
 try:
-    # apps.py
-    from django.apps import AppConfig
-    try:
-        import tagging
-
-        tagging.register(UfsObj)
-    except ImportError:
-        pass
-except ImportError:
-    try:
-        import tagging
-        tagging.register(UfsObj)
-    except ImportError:
-        pass
+    import tagging
+    tagging.register(UfsObj)
 except ImportError:
     pass
