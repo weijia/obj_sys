2cf0e660805efc556a106fbd0a4113d020ba2845
Richard <richardwangwang@gmail.com>
2016-05-08 11:10:31 +0800

    Added tagging app.

--- obj_sys/ufs_obj_in_tree_view.py
+++ obj_sys/ufs_obj_in_tree_view.py
@@ -1,3 +1,5 @@
+from django.contrib.contenttypes.models import ContentType
+
 from mptt_tree_view.views import TreeView
 from obj_sys.models_ufs_obj import UfsObj
 
@@ -9,3 +11,8 @@ class ItemTreeView(TreeView):
     def get_final_query_set(self):
         if self.ufs_obj_type is not None:
             self.tree_items = self.tree_items.filter(ufs_obj_type=self.ufs_obj_type)
+
+    def get_context_data(self, **kwargs):
+        content = super(ItemTreeView, self).get_context_data(**kwargs)
+        content["content_type"] = ContentType.objects.get(model="ufsobj")
+        return content
