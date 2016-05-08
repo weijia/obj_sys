b5eb6bc51afa66d8a177e49dbf91c8245a2bf20c
Richard <richardwangwang@gmail.com>
2016-05-08 18:27:58 +0800

    Remove tree.css. Update tree item html.

--- obj_sys/static/obj_sys/js/obj_sys.js
+++ obj_sys/static/obj_sys/js/obj_sys.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
     $(".tagged-item").taggingApp();
-    $("body").on("click", ".qtip-content", function(e){
-//        console.log("hello world");
-
-    });
+//    $("body").on("click", ".qtip-content", function(e){
+////        console.log("hello world");
+//
+//    });
 });
